63ec750ad856dbc7b2cab729d84460cdd6ee0960
GHLukasLang <langlukas72@gmail.com>
2025-07-03 15:08:06 +0200

    fixed broken code

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-var ErrNoAuthHeaderIncluded = errors.New("no authorization header ")
+var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
