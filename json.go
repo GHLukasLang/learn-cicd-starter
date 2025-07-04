6cca96224a6a63f50c1d96af820ed363457a2168
GHLukasLang <langlukas72@gmail.com>
2025-07-04 12:53:26 +0200

    fixed two sec issues

--- json.go
+++ json.go
@@ -29,6 +29,11 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		w.WriteHeader(500)
 		return
 	}
+
 	w.WriteHeader(code)
-	w.Write(dat)
+	if _, err := w.Write(dat); err != nil {
+		// Log the error, handle it, or at least comment why it's ignored
+		log.Printf("failed to write response: %v", err)
+	}
+
 }
